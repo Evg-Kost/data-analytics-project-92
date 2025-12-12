1a43478659dc796a897bc32bbb9bd494d04b6297
Evg-Kost <e.kostolomov@bk.ru>
2025-12-12 21:58:02 +0300

    Fix lint

--- queries.sql
+++ queries.sql
@@ -7,35 +7,36 @@ select
     FLOOR(SUM(s.quantity * p.price)) as income
 from employees as e
 inner join sales as s
-    on e.employee_id  = s.sales_person_id
+    on e.employee_id = s.sales_person_id
 inner join products as p
-    on s.product_id = p.product_id 
+    on s.product_id = p.product_id
 group by seller
 order by SUM(s.quantity * p.price) desc
 limit 10
 ;
 
 --продавцы с наименьшим средним чеком 
-with avg_sellers as(select 
+with avg_sellers as (select
     e.first_name || ' ' || e.last_name as seller,
-    floor(AVG(s.quantity * p.price)) as average_income,
-    AVG(AVG(s.quantity * p.price)) OVER() avg_average_income
+    FLOOR(AVG(s.quantity * p.price)) as average_income,
+    AVG(AVG(s.quantity * p.price)) over () as avg_average_income
 from employees as e
 inner join sales as s
-    on e.employee_id  = s.sales_person_id
-inner join products as p
-    on s.product_id = p.product_id 
-group by e.employee_id, seller
-order by average_income)
-select 
+    on e.employee_id = s.sales_person_id
+    inner join products as p
+        on s.product_id = p.product_id
+    group by e.employee_id, seller
+    order by average_income
+)
+
+select
     seller,
-    average_income 
-from avg_sellers 
-where average_income < avg_average_income 
-;
+    average_income
+from avg_sellers
+where average_income < avg_average_income;
 
 --продажи продавцов по дням недели
-
+select
     e.first_name || ' ' || e.last_name as seller,
     TRIM(TO_CHAR(s.sale_date, 'day')) as day_of_week,
     floor(SUM(p.price * s.quantity)) as income
