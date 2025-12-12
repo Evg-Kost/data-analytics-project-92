e5444e3cf7d8f8a83dacbae25ad82ad3becc6b77
Evg-Kost <e.kostolomov@bk.ru>
2025-12-12 12:35:02 +0300

    Step 6

--- queries.sql
+++ queries.sql
@@ -13,6 +13,7 @@ group by seller
 order by SUM(s.quantity * p.price) desc
 limit 10
 ;
+
 --продавцы с наименьшим средним чеком 
 with avg_sellers as(select 
 	e.first_name || ' ' || e.last_name as seller,
@@ -31,6 +32,7 @@ select
 from avg_sellers 
 where average_income < avg_average_income 
 ;
+
 --продажи продавцов по дням недели
 select 
 	e.first_name || ' ' || e.last_name as seller,
@@ -43,3 +45,58 @@ inner join products p on
 	s.product_id = p.product_id 
 group by e.employee_id, seller, day_of_week, EXTRACT(ISODOW from s.sale_date)
 order by seller, EXTRACT(ISODOW from s.sale_date);
+
+--разбиение покупателей на группы по возрастам
+SELECT 
+    CASE 
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        WHEN age > 40 THEN '40+'
+        ELSE 'Другое'
+    END as age_category,
+    COUNT(*) as age_count
+FROM customers
+WHERE age IS NOT NULL 
+    AND age >= 16  -- исключаем младше 16, если нужно
+GROUP BY 
+    CASE 
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        WHEN age > 40 THEN '40+'
+        ELSE 'Другое'
+    END
+order by age_category;
+
+--уникальные покупатели и выручка по месяцам
+select
+	to_char(sale_date, 'yyyy-mm') as selling_month,
+	count(distinct customer_id),
+	floor(sum(quantity * price))
+from sales
+inner join products on
+	sales.product_id = products.product_id
+group by to_char(sale_date, 'yyyy-mm')
+order by selling_month;
+--первая покупка по акции для покупателя
+with tab as (select
+	c.first_name || ' ' || c.last_name as customer,
+	s.customer_id,
+	s.sale_date as sale_date,
+	e.first_name || ' ' || e.last_name as seller,
+	p.price,
+	row_number() over(partition by c.first_name || ' ' || c.last_name order by s.sale_date) as rn
+from sales s
+inner join customers c on
+s.customer_id = c.customer_id
+inner join employees e on
+s.sales_person_id = e.employee_id
+inner join products p on 
+s.product_id = p.product_id 
+where p.price = 0
+order by s.customer_id)
+select 
+	customer,
+	sale_date,
+	seller
+from tab
+where rn = 1
